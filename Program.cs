946d22d38ee43dad3f12780e30d8bba3d784d2e7
vitor20022003 <vitorm4123@gmail.com>
2020-12-17 10:53:01 -0300

    POO Listas Concluido

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using POO_Listas.Classes;
+
+namespace POO_Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> produtos = new List<Produto>();
+
+            Produto[] arrayProdutos = {};
+
+            produtos.Add(new Produto(5, "Apple", 2121.21f));
+            produtos.Add(new Produto(8, "Xiaomi", 3211.21f));
+            produtos.Add(new Produto(5, "Motorola", 3124.21f));
+            produtos.Add(new Produto(5, "iPhone", 2761.21f));
+            produtos.Add(new Produto(5, "Samsung", 1452.27f));
+
+            Produto p = new Produto();
+            p.Codigo = 12;
+            p.Nome = "Nokia";
+            p.Preco = 1234.43f;
+
+            produtos.Add(p);
+
+            foreach(Produto item in produtos) {
+                Console.WriteLine(item.Nome);
+            }
+        }
+    }
+}
